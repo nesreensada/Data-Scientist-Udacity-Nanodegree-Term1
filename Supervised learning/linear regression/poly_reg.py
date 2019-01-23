b926b150963b185b8b6b723e25c2f1d86b9662c4
nesreen sada <nesreensada@gmail.com>
2019-01-23 19:02:00 +0200

    add the linear regression from the supervised learning part

---
+++ Supervised learning/linear regression/poly_reg.py
@@ -0,0 +1,25 @@
+# TODO: Add import statements
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv('data.csv', header = 0)
+X = train_data['Var_X'].values.reshape((20,1))   
+y = train_data['Var_Y']
+
+# Create polynomial features
+# TODO: Create a PolynomialFeatures object, then fit and transform the
+# predictor feature
+poly_feat = PolynomialFeatures(degree = 4)
+X_poly = poly_feat.fit_transform(X)
+
+# Make and fit the polynomial regression model
+# TODO: Create a LinearRegression object and fit it to the polynomial predictor
+# features
+poly_model = LinearRegression().fit(X_poly, y)
+
+# Once you've completed all of the steps, select Test Run to see your model
+# predictions against the data, or select Submit Answer to check if the degree
+# of the polynomial features is the same as ours!
